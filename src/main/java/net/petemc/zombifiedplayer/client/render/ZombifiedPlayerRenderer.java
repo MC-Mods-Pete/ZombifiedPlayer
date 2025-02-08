02a0164e57dfa3baec345f27aa2caad2fdddc923
Pete MC-Mods <mcmods.pete@gmail.com>
2025-02-08 23:38:40 +0100

    improve getting gameprofile and player skin

--- src/main/java/net/petemc/zombifiedplayer/client/render/ZombifiedPlayerRenderer.java
+++ src/main/java/net/petemc/zombifiedplayer/client/render/ZombifiedPlayerRenderer.java
@@ -72,7 +72,7 @@ public class ZombifiedPlayerRenderer
     public Identifier getTexture(ZombifiedPlayerEntityRenderState zombieEntityRenderState) {
         if (zombieEntityRenderState.gameProfile != null) {
             if (!ZombifiedPlayerClient.cachedPlayerSkinsByUUID.containsKey(zombieEntityRenderState.gameProfile.getId())) {
-                receivedGameProfile = null;
+
                 getPlayerSkinFromGameProfile(zombieEntityRenderState.gameProfile);
             }
             if (ZombifiedPlayerClient.cachedPlayerSkinsByUUID.containsKey(zombieEntityRenderState.gameProfile.getId())) {
@@ -95,10 +95,9 @@ public class ZombifiedPlayerRenderer
                     receivedGameProfile = getGameProfile(profile);
 
                     if (receivedGameProfile != null) {
-                        counter = 0;
                         ZombifiedPlayer.LOGGER.info("Successfully received GameProfile for {}, UUID: {}", receivedGameProfile.getName(), receivedGameProfile.getId());
-                    } else {
-                        counter--;
+                        counter = counterMax;
+                        totalTries = 0;
                     }
                 }
 
@@ -121,10 +120,11 @@ public class ZombifiedPlayerRenderer
                         ZombifiedPlayer.LOGGER.info("Successfully received Skin for {}, UUID: {}", receivedGameProfile.getName(), receivedGameProfile.getId());
                         ZombifiedPlayer.LOGGER.info("Skin Texture: {}", skinTexture.texture());
                         ZombifiedPlayer.LOGGER.info("Skin Texture URL: {}", skinTexture.textureUrl());
+                        counter = counterMax;
                         totalTries = 0;
+                        receivedGameProfile = null;
                     } else {
                         ZombifiedPlayer.LOGGER.warn("No valid Skin was received for {}", receivedGameProfile.getName());
-                        receivedGameProfile = null;
                     }
                 }
             }
